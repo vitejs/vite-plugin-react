b23dfed46f3941390da5a66f3ec2477360eab58a
Evan You <yyx990803@gmail.com>
2020-12-19 22:33:13 -0500

    test: more test setup

---
+++ scripts/jestGlobalSetup.js
@@ -0,0 +1,24 @@
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
+const { chromium } = require('playwright-chromium')
+
+const DIR = path.join(os.tmpdir(), 'jest_playwright_global_setup')
+
+module.exports = async () => {
+  const browserServer = await chromium.launchServer({
+    args: process.env.CI
+      ? ['--no-sandbox', '--disable-setuid-sandbox']
+      : undefined
+  })
+
+  global.__BROWSER_SERVER__ = browserServer
+
+  fs.mkdirSync(DIR, { recursive: true })
+  fs.writeFileSync(path.join(DIR, 'wsEndpoint'), browserServer.wsEndpoint())
+
+  const fixtureDir = path.resolve(__dirname, '../temp')
+  if (fs.existsSync(fixtureDir)) {
+    fs.rmSync(fixtureDir, { recursive: true })
+  }
+}
