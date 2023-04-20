127075840aa0724ccf2c0e7c6900b14a7772b74b
Arnaud Barré <arnaud.barre@carbometrix.com>
2023-04-20 14:53:49 +0200

    chore: use manual CHANGELOG.md

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,29 +1,33 @@
+import { readFileSync, writeFileSync } from 'node:fs'
 import { release } from '@vitejs/release-scripts'
-import { logRecentCommits, run } from './releaseUtils'
+import colors from 'picocolors'
 
 release({
   repo: 'vite-plugin-react',
   packages: ['plugin-react'],
   toTag: (pkg, version) => `${pkg}@${version}`,
   logChangelog: async (pkgName) => {
-    await logRecentCommits(pkgName)
+    const changelog = readFileSync(`packages/${pkgName}/CHANGELOG.md`, 'utf-8')
+    if (!changelog.includes('## Unreleased')) {
+      throw new Error("Can't find '## Unreleased' section in CHANGELOG.md")
+    }
+    const index = changelog.indexOf('## Unreleased') + 13
+    console.log(
+      colors.dim(
+        changelog.slice(index, changelog.indexOf('## ', index)).trim(),
+      ),
+    )
   },
-  generateChangelog: async (pkgName) => {
-    await run(
-      'npx',
-      [
-        'conventional-changelog',
-        '-p',
-        'angular',
-        '-i',
-        'CHANGELOG.md',
-        '-s',
-        '--commit-path',
-        '.',
-        '--lerna-package',
-        pkgName,
-      ],
-      { cwd: `packages/${pkgName}` },
+  generateChangelog: async (pkgName, version) => {
+    const changelog = readFileSync(`packages/${pkgName}/CHANGELOG.md`, 'utf-8')
+    console.log(colors.dim('Write package.json & CHANGELOG.md'))
+    const date = new Date().toISOString().slice(0, 10)
+    writeFileSync(
+      `packages/${pkgName}/CHANGELOG.md`,
+      changelog.replace(
+        '## Unreleased',
+        `## Unreleased\n\n## ${version} (${date})`,
+      ),
     )
   },
 })
