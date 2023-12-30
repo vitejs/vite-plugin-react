912adbeeba61196037fe66185a7c64396262f239
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-12-30 00:46:34 -0800

    fix: remove `context` from `render`

--- playground/ssr-react/src/entry-server.jsx
+++ playground/ssr-react/src/entry-server.jsx
@@ -2,9 +2,9 @@ import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import { App } from './App'
 
-export function render(url, context) {
+export function render(url) {
   return ReactDOMServer.renderToString(
-    <StaticRouter location={url} context={context}>
+    <StaticRouter location={url}>
       <App />
     </StaticRouter>,
   )
