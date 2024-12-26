ac144fb3e5c4ddfb500aaf3602479d888dbb5812
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-26 15:00:04 +0900

    test: replace react-router example

--- playground/ssr-react/src/entry-server.jsx
+++ playground/ssr-react/src/entry-server.jsx
@@ -1,11 +1,8 @@
 import ReactDOMServer from 'react-dom/server'
-import { StaticRouter } from 'react-router-dom/server'
 import { App } from './App'
 
 export function render(url) {
   return ReactDOMServer.renderToString(
-    <StaticRouter location={url}>
-      <App />
-    </StaticRouter>,
+    <App url={new URL(url, 'http://localhost')} />,
   )
 }
