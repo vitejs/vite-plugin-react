7b25cacd8866801fe73ec0b14d0fc39552aec3e1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-26 13:53:23 +0900

    chore: lint

--- playground/ssr-react/vite.config.js
+++ playground/ssr-react/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import fs from 'node:fs'
 import path from 'node:path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 process.env.MY_CUSTOM_SECRET = 'API_KEY_qwertyuiop'
 
