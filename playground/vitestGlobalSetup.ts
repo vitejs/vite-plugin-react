9920d724dd6442a50497641f37d5ee944c6d2d7b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-04-04 12:33:48 +0900

    chore: setup tests

--- playground/vitestGlobalSetup.ts
+++ playground/vitestGlobalSetup.ts
@@ -43,6 +43,47 @@ export async function setup(): Promise<void> {
         throw error
       }
     })
+
+  // also setup dedicated copy for plugin-react-oxc tests
+  const oxcIgnoredDirs = new Set([
+    'compiler',
+    'compiler-react-18',
+    'react-classic',
+    'react-emotion',
+    'node_modules',
+  ])
+  const oxcPlaygrounds = (
+    await fs.readdir(path.resolve(__dirname, '../playground'), {
+      withFileTypes: true,
+    })
+  ).filter((dirent) => !oxcIgnoredDirs.has(dirent.name) && dirent.isDirectory())
+  for (const { name: playgroundName } of oxcPlaygrounds) {
+    await fs.copy(
+      path.resolve(tempDir, playgroundName),
+      path.resolve(tempDir, `${playgroundName}-oxc`),
+    )
+    await fs.remove(
+      path.resolve(
+        tempDir,
+        `${playgroundName}-oxc/node_modules/@vitejs/plugin-react`,
+      ),
+    )
+    await fs.symlink(
+      path.resolve(__dirname, '../packages/plugin-react-oxc'),
+      path.resolve(
+        tempDir,
+        `${playgroundName}-oxc/node_modules/@vitejs/plugin-react`,
+      ),
+    )
+    await fs.symlink(
+      path.resolve(__dirname, '../packages/plugin-react-oxc/node_modules/vite'),
+      path.resolve(tempDir, `${playgroundName}-oxc/node_modules/vite`),
+    )
+    await fs.copy(
+      path.resolve(__dirname, '../packages/plugin-react-oxc/node_modules/.bin'),
+      path.resolve(tempDir, `${playgroundName}-oxc/node_modules/.bin`),
+    )
+  }
 }
 
 export async function teardown(): Promise<void> {
