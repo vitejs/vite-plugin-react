756adf26c2faa088e8f13896df12d080ec967b46
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-04-30 11:35:26 +0900

    fix: make react preamble inline script async

--- packages/plugin-react-oxc/src/index.ts
+++ packages/plugin-react-oxc/src/index.ts
@@ -140,7 +140,7 @@ export default function viteReact(opts: Options = {}): PluginOption[] {
         return [
           {
             tag: 'script',
-            attrs: { type: 'module' },
+            attrs: { type: 'module', async: 'true' },
             children: getPreambleCode(config.server!.config.base),
           },
         ]
