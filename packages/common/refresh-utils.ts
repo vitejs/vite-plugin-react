e53210baa08cc082d59d55e5ca69e6f537bf55ff
underfin <likui.underfin@gmail.com>
2025-05-09 18:06:02 +0800

    wip: full bundle mode compat

--- packages/common/refresh-utils.ts
+++ packages/common/refresh-utils.ts
@@ -81,7 +81,7 @@ const inWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof 
   newCode = `${sharedHead}${newCode}
 
 if (import.meta.hot && !inWebWorker) {
-  RefreshRuntime.__hmr_import(import.meta.url).then((currentExports) => {
+  import.meta.hot.getExports().then((currentExports) => {
     RefreshRuntime.registerExportsForReactRefresh(${JSON.stringify(
       id,
     )}, currentExports);
