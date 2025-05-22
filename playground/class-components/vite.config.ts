e15582db5db4b692d4f6cc6818e9a510c70e4587
underfin <likui.underfin@gmail.com>
2025-05-22 18:48:09 +0800

    chore: remove advancedChunks reproduction

--- playground/class-components/vite.config.ts
+++ playground/class-components/vite.config.ts
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: { port: 8908 /* Should be unique */ },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        advancedChunks: {
-          groups: [
-            { name: 'node-modules', test: 'node_modules' },
-            { name: 'react', test: 'react' },
-          ],
-        },
-      },
-    },
-  },
 })
