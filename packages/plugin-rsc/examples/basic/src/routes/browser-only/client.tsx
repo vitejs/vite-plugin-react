edefaf926315dd8546bcc0ec795322f0ed6d0049
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-06 11:22:13 +0900

    chore: tweak

--- packages/plugin-rsc/examples/basic/src/routes/browser-only/client.tsx
+++ packages/plugin-rsc/examples/basic/src/routes/browser-only/client.tsx
@@ -56,6 +56,10 @@ const SomeDep = ({ browserOnlyFallback, ...props }) => {
     </React.Suspense>
   )
 }
-*/
 
-// declare function browserOnly<T>(fn: () => Promise<{ default: React.ComponentType<T> }>): React.ComponentType<T & { browserOnlyFallback?: React.ReactNode }>
+=== helper types ===
+
+declare function browserOnly<T>(fn: () => Promise<{ default: React.ComponentType<T> }>):
+  React.ComponentType<T & { browserOnlyFallback?: React.ReactNode }>
+
+*/
