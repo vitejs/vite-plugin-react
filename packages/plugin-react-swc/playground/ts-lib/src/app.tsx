91d61442e0e9c0bc67a155a75860ea780c938f02
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-08 11:02:32 +0900

    chore: cleanup

--- packages/plugin-react-swc/playground/ts-lib/src/app.tsx
+++ packages/plugin-react-swc/playground/ts-lib/src/app.tsx
@@ -3,11 +3,6 @@ import TestNonJs from '@vitejs/test-dep-non-js'
 export default function App() {
   return (
     <section style={{ margin: 24 }}>
-      <header style={{ display: 'flex', gap: 24 }}>
-        {/* <Link to="/">Home</Link>
-        <Link to="/about">About</Link> */}
-      </header>
-
       <main>
         <TestNonJs />
       </main>
