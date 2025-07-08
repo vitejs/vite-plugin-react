54a782fec4f85c15912ed5f550429bc147d697fd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-09 07:54:24 +0900

    test(react-swc): simplify ts-lib test (#548)

--- packages/plugin-react-swc/playground/ts-lib/src/index.tsx
+++ packages/plugin-react-swc/playground/ts-lib/src/index.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Routes } from 'generouted/react-router'
+import App from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Routes />
+    <App />
   </StrictMode>,
 )
