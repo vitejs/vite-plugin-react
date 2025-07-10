ddf3ec6139d9859075c62e058cddfb49845325ae
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-10 21:23:55 +0900

    build: use tsdown for plugin-react-swc

---
+++ packages/plugin-react-swc/tsdown.config.ts
@@ -0,0 +1,57 @@
+import { writeFileSync } from 'node:fs'
+import { defineConfig } from 'tsdown'
+import packageJSON from './package.json'
+
+export default defineConfig({
+  entry: 'src/index.ts',
+  format: ['esm', 'cjs'],
+  dts: true,
+  copy: [
+    {
+      from: 'node_modules/@vitejs/react-common/refresh-runtime.js',
+      to: 'dist/refresh-runtime.js',
+    },
+    {
+      from: 'LICENSE',
+      to: 'dist/LICENSE',
+    },
+    {
+      from: 'README.md',
+      to: 'dist/README.md',
+    },
+  ],
+  outputOptions(outputOpts, format) {
+    if (format === 'cjs') {
+      outputOpts.footer = 'module.exports.default = module.exports'
+    }
+    return outputOpts
+  },
+  onSuccess() {
+    writeFileSync(
+      'dist/package.json',
+      JSON.stringify(
+        {
+          ...Object.fromEntries(
+            Object.entries(packageJSON).filter(
+              ([key, _val]) =>
+                key !== 'devDependencies' &&
+                key !== 'scripts' &&
+                key !== 'private',
+            ),
+          ),
+          main: 'index.cjs',
+          types: 'index.d.ts',
+          module: 'index.js',
+          exports: {
+            '.': {
+              require: './index.cjs',
+              import: './index.js',
+            },
+          },
+        },
+        null,
+        2,
+      ),
+    )
+  },
+})
