dd6f3a8fcfd990cd3619eb4a79b370d6d55a092e
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-11 14:36:56 +0900

    chore: remove `__IS_BUILD__` workaround

--- packages/plugin-react-oxc/tsdown.config.ts
+++ packages/plugin-react-oxc/tsdown.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: 'src/index.ts',
-  define: {
-    'globalThis.__IS_BUILD__': 'true',
-  },
   copy: [
     {
       from: 'node_modules/@vitejs/react-common/refresh-runtime.js',
--- packages/plugin-react/tsdown.config.ts
+++ packages/plugin-react/tsdown.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: 'src/index.ts',
   format: ['esm', 'cjs'],
-  define: {
-    'globalThis.__IS_BUILD__': 'true',
-  },
   copy: [
     {
       from: 'node_modules/@vitejs/react-common/refresh-runtime.js',
