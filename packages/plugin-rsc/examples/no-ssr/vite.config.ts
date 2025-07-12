df27d867f79d6da6dc7088fd41f63565934d46c1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-12 11:46:52 +0900

    test(rsc): add no ssr example

---
+++ packages/plugin-rsc/examples/no-ssr/vite.config.ts
@@ -0,0 +1,65 @@
+import rsc from '@vitejs/plugin-rsc'
+import react from '@vitejs/plugin-react'
+import { defineConfig, type Plugin } from 'vite'
+import fsp from 'node:fs/promises'
+
+export default defineConfig({
+  plugins: [
+    spaPlugin(),
+    react(),
+    rsc({
+      entries: {
+        rsc: './src/framework/entry.rsc.tsx',
+      },
+    }),
+  ],
+})
+
+function spaPlugin(): Plugin[] {
+  return [
+    {
+      name: 'serve-spa',
+      configureServer(server) {
+        // pre-middleware for index.html request
+        return () => {
+          server.middlewares.use(async (req, res, next) => {
+            try {
+              if (req.headers.accept?.includes('text/html')) {
+                const html = await fsp.readFile('index.html', 'utf-8')
+                const transformed = await server.transformIndexHtml('/', html)
+                res.setHeader('Content-type', 'text/html')
+                res.setHeader('Vary', 'accept')
+                res.end(transformed)
+                return
+              }
+            } catch (error) {
+              next(error)
+              return
+            }
+            next()
+          })
+        }
+      },
+      configurePreviewServer(server) {
+        return () => {
+          server.middlewares.use(async (req, res, next) => {
+            try {
+              if (req.headers.accept?.includes('text/html')) {
+                const html = await fsp.readFile(
+                  'dist/client/index.html',
+                  'utf-8',
+                )
+                res.end(html)
+                return
+              }
+            } catch (error) {
+              next(error)
+              return
+            }
+            next()
+          })
+        }
+      },
+    },
+  ]
+}
