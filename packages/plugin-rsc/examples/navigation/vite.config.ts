7afc7cfd4bb9686d0baac85b0ca7b522b295bd7a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-14 10:16:56 +0900

    chore: cleanup

--- packages/plugin-rsc/examples/navigation/vite.config.ts
+++ packages/plugin-rsc/examples/navigation/vite.config.ts
@@ -1,7 +1,6 @@
 import rsc from '@vitejs/plugin-rsc'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
   clearScreen: false,
@@ -15,7 +14,6 @@ export default defineConfig({
       },
       ignoredPackageWarnings: ['navigation-react'],
     }),
-    inspect(),
   ],
   optimizeDeps: {
     include: ['navigation'],
