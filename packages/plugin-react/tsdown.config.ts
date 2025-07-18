3a0b3467f85baf457b1c17f297905747363587cf
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-18 12:54:39 +0900

    refactor!: bump required node version to 20.19+, 22.12+ and drop CJS build

--- packages/plugin-react/tsdown.config.ts
+++ packages/plugin-react/tsdown.config.ts
@@ -2,23 +2,10 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: 'src/index.ts',
-  format: ['esm', 'cjs'],
   copy: [
     {
       from: 'node_modules/@vitejs/react-common/refresh-runtime.js',
       to: 'dist/refresh-runtime.js',
     },
   ],
-  outputOptions(outputOpts, format) {
-    if (format === 'cjs') {
-      outputOpts.footer = (chunk) => {
-        // don't append to dts files
-        if (chunk.fileName.endsWith('.cjs')) {
-          return 'module.exports.default = module.exports'
-        }
-        return ''
-      }
-    }
-    return outputOpts
-  },
 })
