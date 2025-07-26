6d65d6693ff0143d1c989c7a96940c846d838eea
Graham Mendick <graham.mendick@gmail.com>
2025-07-26 19:09:42 +0100

    Copied latest from vite sample in navigation repo
    See https://github.com/grahammendick/navigation/pull/883

--- packages/plugin-rsc/examples/navigation/src/server.ssr.tsx
+++ packages/plugin-rsc/examples/navigation/src/server.ssr.tsx
@@ -1,1 +1,27 @@
-export * from '@vitejs/plugin-rsc/extra/ssr'
+import * as ReactClient from '@vitejs/plugin-rsc/ssr';
+import React from 'react';
+import * as ReactDOMServer from 'react-dom/server.edge';
+import { injectRSCPayload } from 'rsc-html-stream/server';
+
+type RscPayload = {
+  root: React.ReactNode
+}
+export async function renderHTML(
+  rscStream: ReadableStream<Uint8Array>) {
+  const [rscStream1, rscStream2] = rscStream.tee();
+  let payload: Promise<RscPayload>;
+  function SsrRoot() {
+    payload ??= ReactClient.createFromReadableStream<RscPayload>(rscStream1);
+    return React.use(payload).root;
+  }
+  const bootstrapScriptContent =
+    await import.meta.viteRsc.loadBootstrapScriptContent('index');
+  const htmlStream = await ReactDOMServer.renderToReadableStream(<SsrRoot />, {
+    bootstrapScriptContent,
+  });
+  let responseStream: ReadableStream<Uint8Array> = htmlStream;
+  responseStream = responseStream.pipeThrough(
+    injectRSCPayload(rscStream2),
+  );
+  return responseStream;
+}
--- packages/plugin-rsc/examples/navigation/src/server.tsx
+++ packages/plugin-rsc/examples/navigation/src/server.tsx
@@ -1,9 +1,9 @@
-import { renderRequest } from '@vitejs/plugin-rsc/extra/rsc'
-import { StateNavigator } from 'navigation'
-import stateNavigator from './stateNavigator.ts'
+import * as ReactServer from '@vitejs/plugin-rsc/rsc';
+import { StateNavigator } from 'navigation';
+import stateNavigator from './stateNavigator.ts';
 
 export default async function handler(request: Request): Promise<Response> {
-  let url: string;
+  let url: string = '';
   let view: any;
   const serverNavigator = new StateNavigator(stateNavigator);
   if (request.method === 'GET') {
@@ -11,34 +11,35 @@ export default async function handler(request: Request): Promise<Response> {
     url = `${reqUrl.pathname}${reqUrl.search}`;
     const App = (await import('./App.tsx')).default;
     view = <App url={url} />;
-  } else {    
+  }
+  if (request.method === 'POST') {
     const sceneViews: any = {
       people: await import('./People.tsx'),
       person: await import('./Person.tsx'),
       friends: await import('./Friends.tsx')
     };
-    const {url: reqUrl, sceneViewKey, state, data, crumbs} = await request.json();
-    if (reqUrl) {
-      url = reqUrl;
-      const SceneView = sceneViews[sceneViewKey].default;
-      view = <SceneView />;
-    } else {
-      let fluentNavigator = serverNavigator.fluent();
-      for (let i = 0; i < crumbs.length; i++) {
-        fluentNavigator = fluentNavigator.navigate(crumbs[i].state, crumbs[i].data);
-      }
-      fluentNavigator = fluentNavigator.navigate(state, data);
-      url = fluentNavigator.url;
-      const App = (await import('./App.tsx')).default;
-      view = <App url={url} />;
+    const {url: reqUrl, sceneViewKey} = await request.json();
+    url = reqUrl;
+    const SceneView = sceneViews[sceneViewKey].default;
+    view = <SceneView />;
+  }
+  if (request.method === 'PUT') {
+    const {state, data, crumbs} = await request.json();
+    let fluentNavigator = serverNavigator.fluent();
+    for (let i = 0; i < crumbs.length; i++) {
+      fluentNavigator = fluentNavigator.navigate(crumbs[i].state, crumbs[i].data);
     }
+    fluentNavigator = fluentNavigator.navigate(state, data);
+    url = fluentNavigator.url;
+    const App = (await import('./App.tsx')).default;
+    view = <App url={url} />;
   }
   try {
-    serverNavigator.navigateLink(url)
+    serverNavigator.navigateLink(url);
   } catch(e) {
     return new Response('Not Found', { status: 404 });
   }
-  const { NavigationHandler } = await import('navigation-react');
+  const {NavigationHandler} = await import('navigation-react');
   const root = (
     <>
       <NavigationHandler stateNavigator={serverNavigator}>
@@ -46,7 +47,13 @@ export default async function handler(request: Request): Promise<Response> {
       </NavigationHandler>
     </>
   );
-  return renderRequest(request, root);
+  const rscStream = ReactServer.renderToReadableStream({root});
+  if (request.method !== 'GET') {
+    return new Response(rscStream, {headers: {'Content-type': 'text/x-component'}});
+  }
+  const ssrEntryModule = await import.meta.viteRsc.loadModule<typeof import('./server.ssr.tsx')>('ssr', 'index');
+  const htmlStream = await ssrEntryModule.renderHTML(rscStream);
+  return new Response(htmlStream, {headers: {'Content-type': 'text/html'}});
 }
 
 if (import.meta.hot) {
