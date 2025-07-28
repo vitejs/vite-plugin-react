c3bcc2d18d1d4abcb92149263fa9ac1e4d7e70b3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-28 12:35:37 +0900

    fix(rsc): show warning when auto css heuristics failed

--- packages/plugin-rsc/examples/basic/src/routes/style-server/server.tsx
+++ packages/plugin-rsc/examples/basic/src/routes/style-server/server.tsx
@@ -1,13 +1,16 @@
 import './server.css'
 import styles from './server.module.css'
+// import { TestStyleServerNotDetected } from "./not-detected/server";
 
-export function TestStyleServer() {
+export async function TestStyleServer() {
+  const { TestStyleServerNotDetected } = await import('./not-detected/server')
   return (
     <>
       <div className="test-style-server">test-style-server</div>
       <div data-testid="css-module-server" className={styles.server}>
         test-css-module-server
       </div>
+      <TestStyleServerNotDetected />
     </>
   )
 }
