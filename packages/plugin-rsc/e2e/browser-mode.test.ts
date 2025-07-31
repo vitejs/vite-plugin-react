2f809135076cb79746a413bfabe91016f6548a49
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-31 16:18:08 +0900

    test: bye webkit

--- packages/plugin-rsc/e2e/browser-mode.test.ts
+++ packages/plugin-rsc/e2e/browser-mode.test.ts
@@ -3,6 +3,10 @@ import { useFixture } from './fixture'
 import { defineStarterTest } from './starter'
 
 test.describe('dev-browser-mode', () => {
+  // Webkit fails by
+  // > TypeError: ReadableByteStreamController is not implemented
+  test.skip((ctx) => ctx.browserName === 'webkit')
+
   const f = useFixture({ root: 'examples/browser-mode', mode: 'dev' })
   defineStarterTest(f, 'browser-mode')
 })
