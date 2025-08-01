7e6b57a63d033f5bb8d6d27226466e36f0d2be17
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-01 12:36:23 +0900

    test: add e2e

--- packages/plugin-rsc/e2e/browser-mode.test.ts
+++ packages/plugin-rsc/e2e/browser-mode.test.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test, type Page } from '@playwright/test'
 import { useFixture } from './fixture'
 import { defineStarterTest } from './starter'
 
@@ -9,4 +9,66 @@ test.describe('dev-browser-mode', () => {
 
   const f = useFixture({ root: 'examples/browser-mode', mode: 'dev' })
   defineStarterTest(f, 'browser-mode')
+
+  // action-bind tests copied from basic.test.ts
+
+  test('action bind simple', async ({ page }) => {
+    await page.goto(f.url())
+    await testActionBindSimple(page)
+  })
+
+  async function testActionBindSimple(page: Page) {
+    await expect(page.getByTestId('test-server-action-bind-simple')).toHaveText(
+      '[?]',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-simple' })
+      .click()
+    await expect(page.getByTestId('test-server-action-bind-simple')).toHaveText(
+      'true',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-reset' })
+      .click()
+  }
+
+  test('action bind client', async ({ page }) => {
+    await page.goto(f.url())
+    await testActionBindClient(page)
+  })
+
+  async function testActionBindClient(page: Page) {
+    await expect(page.getByTestId('test-server-action-bind-client')).toHaveText(
+      '[?]',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-client' })
+      .click()
+    await expect(page.getByTestId('test-server-action-bind-client')).toHaveText(
+      'true',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-reset' })
+      .click()
+  }
+
+  test('action bind action', async ({ page }) => {
+    await page.goto(f.url())
+    await testActionBindAction(page)
+  })
+
+  async function testActionBindAction(page: Page) {
+    await expect(page.getByTestId('test-server-action-bind-action')).toHaveText(
+      '[?]',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-action' })
+      .click()
+    await expect(page.getByTestId('test-server-action-bind-action')).toHaveText(
+      '[true,true]',
+    )
+    await page
+      .getByRole('button', { name: 'test-server-action-bind-reset' })
+      .click()
+  }
 })
