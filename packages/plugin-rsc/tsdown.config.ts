11d8903750311f500a48fb7c8d36dfe4564ef6b2
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-04 18:39:53 +0900

    fix(rsc): vendor and optimize `use-sync-external-store`

--- packages/plugin-rsc/tsdown.config.ts
+++ packages/plugin-rsc/tsdown.config.ts
@@ -44,6 +44,15 @@ export default defineConfig({
           recursive: true,
           force: true,
         })
+        fs.cpSync(
+          './node_modules/use-sync-external-store',
+          './dist/vendor/use-sync-external-store',
+          { recursive: true, dereference: true },
+        )
+        fs.rmSync('./dist/vendor/use-sync-external-store/node_modules', {
+          recursive: true,
+          force: true,
+        })
       },
     },
   ],
