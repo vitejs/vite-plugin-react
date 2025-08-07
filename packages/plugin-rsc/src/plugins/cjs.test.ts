11c5347824cc01cc874ef044243c810578e6a885
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-07 19:07:26 +0900

    fix(rsc): support cjs on module runner

---
+++ packages/plugin-rsc/src/plugins/cjs.test.ts
@@ -0,0 +1,53 @@
+import { test, expect, describe } from 'vitest'
+import { cjsModuleRunnerTransform } from './cjs'
+import { createServer } from 'vite'
+
+describe(cjsModuleRunnerTransform, () => {
+  test('basic', async () => {
+    const input = `
+exports.ok = true;
+`
+    const output = await cjsModuleRunnerTransform(input)
+    expect(output).toMatchSnapshot()
+    const server = await createServer({
+      configFile: false,
+      plugins: [
+        {
+          name: 'test-entry',
+          resolveId(source) {
+            if (source === 'virtual:test') {
+              return '\0' + source
+            }
+          },
+          load(id) {
+            if (id === '\0virtual:test') {
+              return output
+            }
+          },
+        },
+      ],
+    })
+    const mod = await server.ssrLoadModule('virtual:test')
+    expect(mod).toMatchInlineSnapshot(`
+      {
+        "ok": true,
+      }
+    `)
+  })
+
+  test('basic', async () => {
+    const input = `
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./cjs/use-sync-external-store.production.js');
+} else {
+  module.exports = require('./cjs/use-sync-external-store.development.js');
+}
+`
+    const output = await cjsModuleRunnerTransform(input, {
+      define: {
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      },
+    })
+    expect(output).toMatchSnapshot()
+  })
+})
---
+++ packages/plugin-rsc/src/plugins/cjs.ts
@@ -0,0 +1,103 @@
+import type { Plugin } from 'vite'
+import { parseIdQuery } from './utils'
+import { findClosestPkgJsonPath } from 'vitefu'
+import path from 'node:path'
+import fs from 'node:fs'
+import * as rolldown from 'rolldown'
+
+export function cjsModuleRunnerPlugin(): Plugin[] {
+  return [
+    {
+      name: 'cjs-module-runner-transform',
+      async transform(code, id) {
+        if (
+          id.includes('/node_modules/') &&
+          !id.startsWith(this.environment.config.cacheDir) &&
+          this.environment.config.dev.moduleRunnerTransform &&
+          /\b(require|exports)\b/.test(code)
+        ) {
+          id = parseIdQuery(id).filename
+          let isEsm = id.endsWith('.mjs')
+          if (id.endsWith('.js')) {
+            const pkgJsonPath = await findClosestPkgJsonPath(path.dirname(id))
+            if (pkgJsonPath) {
+              const pkgJson = JSON.parse(
+                fs.readFileSync(pkgJsonPath, 'utf-8'),
+              ) as { type?: string }
+              isEsm = pkgJson.type === 'module'
+            }
+          }
+          if (!isEsm) {
+            // TODO: warning once per package
+            this.warn(
+              `Found non-optimized CJS dependency in '${this.environment.name}' environment. ` +
+                `It is recommended to add the dependency to 'environments.${this.environment.name}.optimizeDeps.include'.`,
+            )
+            return cjsModuleRunnerTransform(code, {
+              define: {
+                'process.env.NODE_ENV': JSON.stringify(
+                  this.environment.config.isProduction
+                    ? 'production'
+                    : 'development',
+                ),
+              },
+            })
+          }
+        }
+      },
+    },
+  ]
+}
+
+export async function cjsModuleRunnerTransform(
+  code: string,
+  config?: rolldown.BuildOptions,
+): Promise<string> {
+  const output = await rolldown.build({
+    ...config,
+    write: false,
+    output: {
+      format: 'esm',
+    },
+    input: 'virtual:entry',
+    plugins: [
+      {
+        name: 'entry',
+        resolveId(source, _importer, options) {
+          if (source === 'virtual:entry') {
+            return '\0' + source
+          }
+          if (source === 'virtual:entry-inner') {
+            return '\0' + source
+          }
+          if (options.kind === 'require-call') {
+            return '\0virtual:require-to-import/' + source
+          }
+          return {
+            id: source,
+            external: true,
+          }
+        },
+        load(id) {
+          if (id === '\0virtual:entry') {
+            return `
+import m from "virtual:entry-inner";
+__vite_ssr_exportAll__(m);
+`
+          }
+          if (id === '\0virtual:entry-inner') {
+            return code
+          }
+          if (id.startsWith('\0virtual:require-to-import/')) {
+            id = id.slice('\0virtual:require-to-import/'.length)
+            return `
+import m from ${JSON.stringify(id)};
+module.exports = m;
+`
+          }
+        },
+      },
+    ],
+  })
+  return output.output[0].code
+}
