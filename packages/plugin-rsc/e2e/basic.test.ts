ae05b9345f68dc31399a292d83a4d6ce3d280c88
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-12 14:41:36 +0900

    test: tweak

--- packages/plugin-rsc/e2e/basic.test.ts
+++ packages/plugin-rsc/e2e/basic.test.ts
@@ -473,10 +473,10 @@ function defineTest(f: Fixture) {
         'color',
         'rgb(255, 165, 0)',
       )
-      await expectNoServerLinkStylesheet(page)
+      await expectNoDuplicateServerCss(page)
     })
 
-    async function expectNoServerLinkStylesheet(page: Page) {
+    async function expectNoDuplicateServerCss(page: Page) {
       // check only manually inserted stylesheet link exists
       // (toHaveAttribute passes only when locator matches single element)
       await expect(page.locator('link[rel="stylesheet"]')).toHaveAttribute(
@@ -485,10 +485,10 @@ function defineTest(f: Fixture) {
       )
     }
 
-    test('no duplicate css', async ({ page }) => {
+    test('no duplicate server css', async ({ page }) => {
       await page.goto(f.url())
       await waitForHydration(page)
-      await expectNoServerLinkStylesheet(page)
+      await expectNoDuplicateServerCss(page)
     })
 
     test('adding/removing css client @js', async ({ page }) => {
@@ -573,7 +573,7 @@ function defineTest(f: Fixture) {
         'color',
         'rgb(255, 165, 0)',
       )
-      await expectNoServerLinkStylesheet(page)
+      await expectNoDuplicateServerCss(page)
     })
 
     // TODO: need a way to add/remove links on server hmr. for now, it requires a manually reload.
