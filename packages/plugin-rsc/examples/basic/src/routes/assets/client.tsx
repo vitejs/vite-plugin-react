74108a9d95c26de707b5db23064dffdeef32841f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-18 12:44:06 +0900

    chore: cleanup

--- packages/plugin-rsc/examples/basic/src/routes/assets/client.tsx
+++ packages/plugin-rsc/examples/basic/src/routes/assets/client.tsx
@@ -7,8 +7,12 @@ export function TestAssetsClient() {
   return (
     <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
       <span>test-assets-client</span>
-      <img src={svg} data-testid="test-assets-client-js" width="20" />
-      {/* TODO? */}
+      <img
+        src={svg}
+        data-testid="test-assets-client-js"
+        width="20"
+        height="20"
+      />
       <span className="test-assets-client-css" />
     </div>
   )
