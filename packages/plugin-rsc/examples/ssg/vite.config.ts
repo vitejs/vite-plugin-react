f66b3c9a638af6feeea475a5aa3094b190a52dd3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-27 09:25:03 +0900

    fix: ssg

--- packages/plugin-rsc/examples/ssg/vite.config.ts
+++ packages/plugin-rsc/examples/ssg/vite.config.ts
@@ -25,6 +25,14 @@ export default defineConfig((env) => ({
     }),
     rscSsgPlugin(),
   ],
+  environments: {
+    ssr: {
+      optimizeDeps: {
+        // TODO: move to plugin
+        include: ['react-dom/static.edge'],
+      },
+    },
+  },
 }))
 
 function rscSsgPlugin(): Plugin[] {
