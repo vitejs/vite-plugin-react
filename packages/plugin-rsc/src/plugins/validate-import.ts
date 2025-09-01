4da58106e9c2ba1258ff3f97e853324af24f4ed8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-01 16:02:41 +0900

    fix(rsc): delay `validateImportPlugin` setup (#813)

--- packages/plugin-rsc/src/plugins/validate-import.ts
+++ packages/plugin-rsc/src/plugins/validate-import.ts
@@ -3,9 +3,12 @@ import type { Plugin } from 'vite'
 // https://github.com/vercel/next.js/blob/90f564d376153fe0b5808eab7b83665ee5e08aaf/packages/next/src/build/webpack-config.ts#L1249-L1280
 // https://github.com/pcattori/vite-env-only/blob/68a0cc8546b9a37c181c0b0a025eb9b62dbedd09/src/deny-imports.ts
 // https://github.com/sveltejs/kit/blob/84298477a014ec471839adf7a4448d91bc7949e4/packages/kit/src/exports/vite/index.js#L513
-export function validateImportPlugin(): Plugin {
+export function validateImportPlugin(
+  applyOptions: Pick<Plugin, 'apply'>,
+): Plugin {
   return {
     name: 'rsc:validate-imports',
+    ...applyOptions,
     resolveId: {
       order: 'pre',
       async handler(source, importer, options) {
