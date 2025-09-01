3969f8602cf43de95d6ae086a0612188d56a239d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-01 16:18:00 +0900

    refactor(rsc): simplify `validateImportPlugin` (#814)

--- packages/plugin-rsc/src/plugin.ts
+++ packages/plugin-rsc/src/plugin.ts
@@ -1007,9 +1007,10 @@ import.meta.hot.on("rsc:update", () => {
     ...vitePluginRscMinimal(rscPluginOptions, manager),
     ...vitePluginFindSourceMapURL(),
     ...vitePluginRscCss(rscPluginOptions, manager),
-    validateImportPlugin({
+    {
+      ...validateImportPlugin(),
       apply: () => rscPluginOptions.validateImports !== false,
-    }),
+    },
     scanBuildStripPlugin({ manager }),
     ...cjsModuleRunnerPlugin(),
     ...globalAsyncLocalStoragePlugin(),
