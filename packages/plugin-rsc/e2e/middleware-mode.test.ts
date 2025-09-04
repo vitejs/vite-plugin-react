ba98b4566457c58d4dc26289b804730e37f8b7bc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-04 11:24:38 +0800

    test: add e2e

--- packages/plugin-rsc/e2e/middleware-mode.test.ts
+++ packages/plugin-rsc/e2e/middleware-mode.test.ts
@@ -3,58 +3,38 @@ import { setupInlineFixture, useFixture } from './fixture'
 import { defineStarterTest } from './starter'
 
 test.describe(() => {
-  const root = 'examples/e2e/temp/module-runner-hmr-false'
+  const root = 'examples/e2e/temp/middleware-mode'
 
   test.beforeAll(async () => {
     await setupInlineFixture({
       src: 'examples/starter',
       dest: root,
       files: {
-        'vite.config.base.ts': { cp: 'vite.config.ts' },
-        'vite.config.ts': /* js */ `
-          import { defineConfig, mergeConfig, createRunnableDevEnvironment } from 'vite'
-          import baseConfig from './vite.config.base.ts'
-
-          const overrideConfig = defineConfig({
-            environments: {
-              ssr: {
-                dev: {
-                  createEnvironment(name, config) {
-                    return createRunnableDevEnvironment(name, config, {
-                      runnerOptions: {
-                        hmr: false,
-                      },
-                    })
-                  },
-                },
-              },
-              rsc: {
-                dev: {
-                  createEnvironment(name, config) {
-                    return createRunnableDevEnvironment(name, config, {
-                      runnerOptions: {
-                        hmr: false,
-                      },
-                    })
-                  },
-                },
-              },
-            },
-          })
-
-          export default mergeConfig(baseConfig, overrideConfig)
-        `,
+        'middleware-mode.ts': { cp: '../../middleware-mode.ts' },
       },
     })
   })
 
   test.describe('dev-middleware-mode', () => {
-    const f = useFixture({ root, mode: 'dev' })
+    const f = useFixture({
+      root,
+      mode: 'dev',
+      command: 'node ./middleware-mode.ts dev',
+    })
     defineStarterTest(f)
   })
 
   test.describe('build-middleware-mode', () => {
-    const f = useFixture({ root, mode: 'build' })
+    const f = useFixture({
+      root,
+      mode: 'build',
+      command: 'node ./middleware-mode.ts start',
+      cliOptions: {
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    })
     defineStarterTest(f)
   })
 })
