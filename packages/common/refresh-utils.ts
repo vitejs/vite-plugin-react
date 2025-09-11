3a8b4a9070399bc7518cfcceb64d1518fda1568a
翠 <green@sapphi.red>
2025-09-11 21:15:17 +0900

    perf: avoid sourcemap for refresh wrapper injection (#835)

--- packages/common/refresh-utils.ts
+++ packages/common/refresh-utils.ts
@@ -14,33 +14,22 @@ window.$RefreshSig$ = () => (type) => type;`
 export const getPreambleCode = (base: string): string =>
   preambleCode.replace('__BASE__', base)
 
-export const avoidSourceMapOption = Symbol()
-
-export function addRefreshWrapper<M extends { mappings: string }>(
+export function addRefreshWrapper(
   code: string,
-  map: M | string | typeof avoidSourceMapOption,
   pluginName: string,
   id: string,
   reactRefreshHost = '',
-): { code: string; map: M | null | string } {
+): string | undefined {
   const hasRefresh = refreshContentRE.test(code)
   const onlyReactComp = !hasRefresh && reactCompRE.test(code)
-  const normalizedMap = map === avoidSourceMapOption ? null : map
 
-  if (!hasRefresh && !onlyReactComp) return { code, map: normalizedMap }
-
-  const avoidSourceMap = map === avoidSourceMapOption
-  const newMap =
-    typeof normalizedMap === 'string'
-      ? (JSON.parse(normalizedMap) as M)
-      : normalizedMap
+  if (!hasRefresh && !onlyReactComp) return undefined
 
   let newCode = code
-  if (hasRefresh) {
-    const refreshHead = removeLineBreaksIfNeeded(
-      `let prevRefreshReg;
-let prevRefreshSig;
+  newCode += `
 
+import * as RefreshRuntime from "${reactRefreshHost}${runtimePublicPath}";
+const inWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope;
 if (import.meta.hot && !inWebWorker) {
   if (!window.$RefreshReg$) {
     throw new Error(
@@ -48,39 +37,6 @@ if (import.meta.hot && !inWebWorker) {
     );
   }
 
-  prevRefreshReg = window.$RefreshReg$;
-  prevRefreshSig = window.$RefreshSig$;
-  window.$RefreshReg$ = RefreshRuntime.getRefreshReg(${JSON.stringify(id)});
-  window.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
-}
-
-`,
-      avoidSourceMap,
-    )
-
-    newCode = `${refreshHead}${newCode}
-
-if (import.meta.hot && !inWebWorker) {
-  window.$RefreshReg$ = prevRefreshReg;
-  window.$RefreshSig$ = prevRefreshSig;
-}
-`
-    if (newMap) {
-      newMap.mappings = ';'.repeat(16) + newMap.mappings
-    }
-  }
-
-  const sharedHead = removeLineBreaksIfNeeded(
-    `import * as RefreshRuntime from "${reactRefreshHost}${runtimePublicPath}";
-const inWebWorker = typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope;
-
-`,
-    avoidSourceMap,
-  )
-
-  newCode = `${sharedHead}${newCode}
-
-if (import.meta.hot && !inWebWorker) {
   RefreshRuntime.__hmr_import(import.meta.url).then((currentExports) => {
     RefreshRuntime.registerExportsForReactRefresh(${JSON.stringify(
       id,
@@ -95,13 +51,12 @@ if (import.meta.hot && !inWebWorker) {
   });
 }
 `
-  if (newMap) {
-    newMap.mappings = ';;;' + newMap.mappings
+
+  if (hasRefresh) {
+    newCode += `function $RefreshReg$(type, id) { return RefreshRuntime.register(type, ${JSON.stringify(id)} + ' ' + id) }
+function $RefreshSig$() { return RefreshRuntime.createSignatureFunctionForTransform(); }
+`
   }
 
-  return { code: newCode, map: newMap }
-}
-
-function removeLineBreaksIfNeeded(code: string, enabled: boolean): string {
-  return enabled ? code.replace(/\n/g, '') : code
+  return newCode
 }
