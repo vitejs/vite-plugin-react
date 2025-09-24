fbb93098db89f466b67c58da0472b57758119770
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-09-24 18:36:32 +0900

    chore: add comment

--- packages/plugin-react/src/index.ts
+++ packages/plugin-react/src/index.ts
@@ -362,6 +362,7 @@ export default function viteReact(opts: Options = {}): Plugin[] {
 
       let nativePlugin: ((options: any) => Plugin) | undefined
       try {
+        // NOTE: `+` is to bypass lint & typecheck. vite/internal exists for newer rolldown-vite
         nativePlugin = (await import('vite' + '/internal'))
           .reactRefreshWrapperPlugin
       } catch {}
