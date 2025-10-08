68e6551f9c88e8d33134ba9659311ac3adf5a060
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-08 17:08:40 +0900

    test: add e2e

--- packages/plugin-rsc/e2e/isolated.test.ts
+++ packages/plugin-rsc/e2e/isolated.test.ts
@@ -1,10 +1,11 @@
-import { test } from '@playwright/test'
+import { expect, test, type Page } from '@playwright/test'
 import { setupIsolatedFixture, useFixture } from './fixture'
 import { defineStarterTest } from './starter'
 import path from 'node:path'
 import os from 'node:os'
 import * as vite from 'vite'
 import { waitForHydration } from './helper'
+import { x } from 'tinyexec'
 
 test.describe(() => {
   // use RUNNER_TEMP on Github Actions
@@ -20,6 +21,10 @@ test.describe(() => {
   test.describe('dev-isolated', () => {
     const f = useFixture({ root: tmpRoot, mode: 'dev' })
     defineStarterTest(f)
+
+    test('verify react-server-dom-webpack', async ({ page }) => {
+      await testReactServerDom(page, f.url(), true)
+    })
   })
 
   test.describe('build-isolated', () => {
@@ -86,3 +91,76 @@ test.describe('examples/ssg', () => {
     })
   })
 })
+
+test.describe('react-server-dom-webpack', () => {
+  const tmpRoot = path.join(
+    process.env['RUNNER_TEMP'] || os.tmpdir(),
+    'test-vite-rsc-webpack',
+  )
+  test.beforeAll(async () => {
+    await setupIsolatedFixture({
+      src: 'examples/starter',
+      dest: tmpRoot,
+      overrides: {
+        vite: '^6',
+      },
+    })
+    const version = (await import('react')).version
+    await x('pnpm', ['i', `react-server-dom-webpack@${version}`], {
+      throwOnError: true,
+      nodeOptions: {
+        cwd: tmpRoot,
+      },
+    })
+  })
+
+  test.describe('dev', () => {
+    const f = useFixture({ root: tmpRoot, mode: 'dev' })
+    defineStarterTest(f)
+
+    test('verify react-server-dom-webpack', async ({ page }) => {
+      await testReactServerDom(page, f.url(), false)
+    })
+  })
+
+  test.describe('build', () => {
+    const f = useFixture({ root: tmpRoot, mode: 'build' })
+    defineStarterTest(f)
+  })
+})
+
+async function testReactServerDom(
+  page: Page,
+  url: string,
+  expectVendor: boolean,
+) {
+  let hasVendor = false
+  let hasNonVendor = false
+  page.on('request', async (request) => {
+    if (
+      request
+        .url()
+        .includes('.vite/deps/react-server-dom-webpack_client__browser.js')
+    ) {
+      hasNonVendor = true
+    }
+    if (
+      request
+        .url()
+        .includes(
+          '.vite/deps/@vitejs_plugin-rsc_vendor_react-server-dom_client__browser.js',
+        )
+    ) {
+      hasVendor = true
+    }
+  })
+  await page.goto(url)
+  await waitForHydration(page)
+  if (expectVendor) {
+    expect(hasVendor).toBe(true)
+    expect(hasNonVendor).toBe(false)
+  } else {
+    expect(hasVendor).toBe(false)
+    expect(hasNonVendor).toBe(true)
+  }
+}
