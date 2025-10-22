49966c414840178756e6b10e5158a772368fadc7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-22 11:41:43 +0900

    fix: patch `rsc:inject-async-local-storage`

--- packages/plugin-rsc/examples/browser-mode2/vite.config.ts
+++ packages/plugin-rsc/examples/browser-mode2/vite.config.ts
@@ -1,96 +1,42 @@
 import { defineConfig, type Plugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import rsc from '@vitejs/plugin-rsc'
-import fsp from 'node:fs/promises'
 
 export default defineConfig({
+  appType: 'spa',
   plugins: [
-    spaPlugin(),
     react(),
     rsc({
+      serverHandler: false,
       entries: {
         rsc: './src/framework/entry.rsc.tsx',
       },
     }),
     rscBrowserMode2Plugin(),
+    {
+      name: 'patch-rsc',
+      configResolved(config) {
+        const plugin = config.plugins.find(
+          (p) => p.name === 'rsc:inject-async-local-storage',
+        )
+        delete plugin!.transform
+      },
+    },
   ],
-  define: {
-    'process.env.NODE_ENV': JSON.stringify('development'),
-  },
   environments: {
     rsc: {
+      keepProcessEnv: false,
       resolve: {
         noExternal: true,
-        alias: {
-          'node:async_hooks': '/src/framework/async-hooks-polyfill.js',
-        },
       },
       optimizeDeps: {
-        include: [
-          'react',
-          'react-dom',
-          'react/jsx-runtime',
-          'react/jsx-dev-runtime',
-          '@vitejs/plugin-rsc/vendor/react-server-dom/server.edge',
-          '@vitejs/plugin-rsc/vendor/react-server-dom/client.edge',
-        ],
-        exclude: ['@vitejs/plugin-rsc'],
         esbuildOptions: {
-          platform: 'browser',
+          platform: 'neutral',
         },
       },
     },
   },
 })
-
-function spaPlugin(): Plugin[] {
-  // serve index.html before rsc server
-  return [
-    {
-      name: 'serve-spa',
-      configureServer(server) {
-        return () => {
-          server.middlewares.use(async (req, res, next) => {
-            try {
-              if (req.headers.accept?.includes('text/html')) {
-                const html = await fsp.readFile('index.html', 'utf-8')
-                const transformed = await server.transformIndexHtml('/', html)
-                res.setHeader('Content-type', 'text/html')
-                res.setHeader('Vary', 'accept')
-                res.end(transformed)
-                return
-              }
-            } catch (error) {
-              next(error)
-              return
-            }
-            next()
-          })
-        }
-      },
-      configurePreviewServer(server) {
-        return () => {
-          server.middlewares.use(async (req, res, next) => {
-            try {
-              if (req.headers.accept?.includes('text/html')) {
-                const html = await fsp.readFile(
-                  'dist/client/index.html',
-                  'utf-8',
-                )
-                res.end(html)
-                return
-              }
-            } catch (error) {
-              next(error)
-              return
-            }
-            next()
-          })
-        }
-      },
-    },
-  ]
-}
 
 function rscBrowserMode2Plugin(): Plugin[] {
   return [
