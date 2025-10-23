d575a2dcb2f752815139fb742f1ae5db4c71be11
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-23 16:40:49 +0900

    chore: update browser-mode too

--- packages/plugin-rsc/examples/browser-mode/src/framework/load-client-dev.tsx
+++ packages/plugin-rsc/examples/browser-mode/src/framework/load-client-dev.tsx
@@ -1,19 +1,17 @@
 import { ESModulesEvaluator, ModuleRunner } from 'vite/module-runner'
+import { createRPCClient } from 'vite-dev-rpc'
+
+const rpcClient = createRPCClient<{ invoke: Function }, {}>(
+  'transport-proxy',
+  import.meta.hot!,
+)
 
 export default async function loadClient() {
   const runner = new ModuleRunner(
     {
       sourcemapInterceptor: false,
       transport: {
-        invoke: async (payload) => {
-          const response = await fetch(
-            '/@vite/invoke-react-client?' +
-              new URLSearchParams({
-                data: JSON.stringify(payload),
-              }),
-          )
-          return response.json()
-        },
+        invoke: (payload) => rpcClient.invoke(payload),
       },
       hmr: false,
     },
