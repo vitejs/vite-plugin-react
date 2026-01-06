0e7ce023eff8cbe04b6d2ce16d8bf1b49105338c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-06 12:46:40 +0900

    fix: fix dts

--- packages/plugin-rsc/types/index.d.ts
+++ packages/plugin-rsc/types/index.d.ts
@@ -2,7 +2,7 @@ declare global {
   interface ImportMeta {
     readonly viteRsc: {
       loadCss: (importer?: string) => import('react').ReactNode
-      loadModule: <T>(environmentName: string, entryName: string) => Promise<T>
+      loadModule: <T>(environmentName: string, entryName?: string) => Promise<T>
       loadBootstrapScriptContent: (entryName: string) => Promise<string>
     }
   }
