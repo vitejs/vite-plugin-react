94532a5597bf8e6fc74d160acbd3245c67077f82
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-16 19:25:25 +0900

    test: add e2e

---
+++ packages/plugin-rsc/e2e/import-environment.test.ts
@@ -0,0 +1,46 @@
+import { test } from '@playwright/test'
+import { setupInlineFixture, useFixture } from './fixture'
+import { defineStarterTest } from './starter'
+
+test.describe('viteRsc.import', () => {
+  const root = 'examples/e2e/temp/vitersc-import'
+  test.beforeAll(async () => {
+    await setupInlineFixture({
+      src: 'examples/starter',
+      dest: root,
+      files: {
+        'src/framework/entry.rsc.tsx': {
+          edit: (s) =>
+            s.replace(
+              `\
+  const ssrEntryModule = await import.meta.viteRsc.loadModule<
+    typeof import('./entry.ssr.tsx')
+  >('ssr', 'index')
+`,
+              `\
+  const ssrEntryModule = await import.meta.viteRsc.import<
+    typeof import('./entry.ssr.tsx')
+  >('./entry.ssr.tsx', { environment: 'ssr' })
+`,
+            ),
+        },
+        'vite.config.base.ts': { cp: 'vite.config.ts' },
+        'vite.config.ts': /* js */ `
+          import baseConfig from './vite.config.base.ts'
+          delete baseConfig.environments.ssr.build.rollupOptions.input;
+          export default baseConfig;
+        `,
+      },
+    })
+  })
+
+  test.describe('dev', () => {
+    const f = useFixture({ root, mode: 'dev' })
+    defineStarterTest(f)
+  })
+
+  test.describe('build', () => {
+    const f = useFixture({ root, mode: 'build' })
+    defineStarterTest(f)
+  })
+})
