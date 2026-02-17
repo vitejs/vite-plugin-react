ccb84df3671fc303fa28420da88b83303360a46b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-02-17 18:55:45 +0900

    wip: plugin-react without babel

--- playground/compiler-react-18/vite.config.ts
+++ playground/compiler-react-18/vite.config.ts
@@ -1,16 +1,62 @@
+import babel from '@rollup/plugin-babel'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, withFilter } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   return {
     server: { port: 8901 /* Should be unique */ },
     plugins: [
-      react({
-        babel: {
-          plugins: [['babel-plugin-react-compiler', { target: '18' }]],
+      {
+        ...withFilter(
+          babel({
+            babelrc: false,
+            configFile: false,
+            plugins: [['babel-plugin-react-compiler', { target: '18' }]],
+            parserOpts: {
+              sourceType: 'module',
+              allowAwaitOutsideFunction: true,
+            },
+            overrides: [
+              {
+                test: '**/*.jsx',
+                parserOpts: { plugins: ['jsx'] },
+              },
+              {
+                test: '**/*.ts',
+                parserOpts: { plugins: ['typescript'] },
+              },
+              {
+                test: '**/*.tsx',
+                parserOpts: { plugins: ['typescript', 'jsx'] },
+              },
+            ],
+
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            babelHelpers: 'bundled',
+            skipPreflightCheck: true,
+          }),
+          {
+            transform: {
+              // use `/['"]use memo['"]/` instead for `compilationMode: "annotation"`
+              code: /\b[A-Z]|\buse/,
+              id: { exclude: /\/node_modules\// },
+            },
+          },
+        ),
+        config() {
+          return {
+            optimizeDeps: {
+              include: ['react-compiler-runtime'],
+            },
+          }
         },
-      }),
+        applyToEnvironment(environment) {
+          return environment.name === 'client'
+        },
+        enforce: 'pre',
+      },
+      react(),
     ],
   }
 })
--- playground/compiler/vite.config.ts
+++ playground/compiler/vite.config.ts
@@ -1,15 +1,60 @@
+import babel from '@rollup/plugin-babel'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, withFilter } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const babelPlugins = [['babel-plugin-react-compiler', {}]]
-  if (command === 'serve') {
-    babelPlugins.push(['@babel/plugin-transform-react-jsx-development', {}])
-  }
+export default defineConfig({
+  server: { port: 8900 /* Should be unique */ },
+  plugins: [
+    {
+      ...withFilter(
+        babel({
+          babelrc: false,
+          configFile: false,
+          plugins: [['babel-plugin-react-compiler', {}]],
+          parserOpts: {
+            sourceType: 'module',
+            allowAwaitOutsideFunction: true,
+          },
+          overrides: [
+            {
+              test: '**/*.jsx',
+              parserOpts: { plugins: ['jsx'] },
+            },
+            {
+              test: '**/*.ts',
+              parserOpts: { plugins: ['typescript'] },
+            },
+            {
+              test: '**/*.tsx',
+              parserOpts: { plugins: ['typescript', 'jsx'] },
+            },
+          ],
 
-  return {
-    server: { port: 8900 /* Should be unique */ },
-    plugins: [react({ babel: { plugins: babelPlugins } })],
-  }
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          babelHelpers: 'bundled',
+          skipPreflightCheck: true,
+        }),
+        {
+          transform: {
+            // use `/['"]use memo['"]/` instead for `compilationMode: "annotation"`
+            code: /\b[A-Z]|\buse/,
+            id: { exclude: /\/node_modules\// },
+          },
+        },
+      ),
+      config() {
+        return {
+          optimizeDeps: {
+            include: ['react-compiler-runtime'],
+          },
+        }
+      },
+      applyToEnvironment(environment) {
+        return environment.name === 'client'
+      },
+      enforce: 'pre',
+    },
+    react(),
+  ],
 })
