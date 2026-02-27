557b801710edd256fd02a62a0811721a5755c78a
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-02-27 16:06:04 +0900

    test: fix rsc react compiler test

--- packages/plugin-rsc/e2e/react-compiler.test.ts
+++ packages/plugin-rsc/e2e/react-compiler.test.ts
@@ -1,4 +1,6 @@
+import { readFileSync } from 'node:fs'
 import { expect, test } from '@playwright/test'
+import { x } from 'tinyexec'
 import { setupInlineFixture, useFixture } from './fixture'
 import { waitForHydration } from './helper'
 import { defineStarterTest } from './starter'
@@ -13,22 +15,39 @@ test.describe(() => {
       files: {
         'vite.config.base.ts': { cp: 'vite.config.ts' },
         'vite.config.ts': /* js */ `
+          import babel from '@rolldown/plugin-babel'
           import rsc from '@vitejs/plugin-rsc'
-          import react from '@vitejs/plugin-react'
+          import react, { reactCompilerPreset } from '@vitejs/plugin-react'
           import { defineConfig, mergeConfig } from 'vite'
           import baseConfig from './vite.config.base.ts'
-          
+
           delete baseConfig.plugins
 
           const overrideConfig = defineConfig({
             plugins: [
-              react({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
+              react(),
+              babel({ presets: [reactCompilerPreset()] }),
               rsc(),
             ],
           })
 
           export default mergeConfig(baseConfig, overrideConfig)
         `,
+      },
+    })
+    const { version } = JSON.parse(
+      readFileSync(
+        new URL(
+          '../package.json',
+          import.meta.resolve('@rolldown/plugin-babel'),
+        ),
+        'utf-8',
+      ),
+    )
+    await x('pnpm', ['i', `@rolldown/plugin-babel@${version}`], {
+      throwOnError: true,
+      nodeOptions: {
+        cwd: root,
       },
     })
   })
