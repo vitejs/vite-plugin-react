9446aa075b79c737f6ceda4835db2bad715211e6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-21 17:18:39 +0900

    chore: more types

--- packages/plugin-rsc/src/types/index.ts
+++ packages/plugin-rsc/src/types/index.ts
@@ -29,34 +29,52 @@ export type CallServerCallback = (id: string, args: unknown[]) => unknown
 // Best-effort latest RSC API types
 // https://github.com/wakujs/waku/blob/2ce74ee2381f6c0593b8246f33043434706889fe/packages/waku/src/lib/react-types.d.ts
 
+// https://github.com/facebook/react/blob/8b2e903a7447d370eb77bb117bc4c0ae240ce831/packages/react-server-dom-webpack/src/server/ReactFlightDOMServerEdge.js#L64-L73
 export interface RenderToReadableStreamOptions {
   debugChannel?: { readable?: ReadableStream; writable?: WritableStream }
-  environmentName?: string
+  environmentName?: string | (() => string)
+  filterStackFrame?: (url: string, functionName: string) => boolean
   identifierPrefix?: string
   signal?: AbortSignal
+  startTime?: number
   temporaryReferences?: ServerTemporaryReferenceSet
   onError?: (error: unknown) => void
-  onPostpone?: (reason: string) => void
 }
 
+// https://github.com/facebook/react/blob/8b2e903a7447d370eb77bb117bc4c0ae240ce831/packages/react-server-dom-webpack/src/client/ReactFlightDOMClientBrowser.js#L47-L57
 export interface CreateFromReadableStreamBrowserOptions {
+  callServer?: CallServerCallback
   debugChannel?: { writable?: WritableStream; readable?: ReadableStream }
-  temporaryReferences?: ClientTemporaryReferenceSet
-}
-
-export interface CreateFromReadableStreamEdgeOptions {
-  nonce?: string
+  endTime?: number
   environmentName?: string
+  findSourceMapURL?: (filename: string, environmentName: string) => string
   replayConsoleLogs?: boolean
+  startTime?: number
   temporaryReferences?: ClientTemporaryReferenceSet
 }
 
+// https://github.com/facebook/react/blob/8b2e903a7447d370eb77bb117bc4c0ae240ce831/packages/react-server-dom-webpack/src/client/ReactFlightDOMClientEdge.js#L74-L87
+export interface CreateFromReadableStreamEdgeOptions {
+  debugChannel?: { readable?: ReadableStream }
+  endTime?: number
+  environmentName?: string
+  findSourceMapURL?: (filename: string, environmentName: string) => string
+  nonce?: string
+  replayConsoleLogs?: boolean
+  startTime?: number
+  temporaryReferences?: ClientTemporaryReferenceSet
+}
+
+// https://github.com/facebook/react/blob/8b2e903a7447d370eb77bb117bc4c0ae240ce831/packages/react-server-dom-webpack/src/server/ReactFlightDOMServerEdge.js#L247-L253
 export interface DecodeReplyOptions {
   temporaryReferences?: ServerTemporaryReferenceSet
+  arraySizeLimit?: number
 }
 
+// https://github.com/facebook/react/blob/8b2e903a7447d370eb77bb117bc4c0ae240ce831/packages/react-server-dom-webpack/src/client/ReactFlightDOMClientBrowser.js#L261-L263
 export interface EncodeReplyOptions {
   temporaryReferences?: ClientTemporaryReferenceSet
+  signal?: AbortSignal
 }
 
 // TODO: for now keep them unknown
