55e36a056c4a1e815c6df282234a8480525ed361
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-04-27 12:27:24 +0900

    test: replace starter-cf-single in favor of starter-extra

--- packages/plugin-rsc/e2e/cloudflare.test.ts
+++ packages/plugin-rsc/e2e/cloudflare.test.ts
@@ -3,11 +3,20 @@ import { useFixture } from './fixture'
 import { defineStarterTest } from './starter'
 
 test.describe('dev-cloudflare', () => {
-  const f = useFixture({ root: 'examples/starter-cf-single', mode: 'dev' })
+  const f = useFixture({
+    root: 'examples/starter-extra',
+    mode: 'dev',
+    command: 'pnpm cf:dev',
+  })
   defineStarterTest(f)
 })
 
 test.describe('build-cloudflare', () => {
-  const f = useFixture({ root: 'examples/starter-cf-single', mode: 'build' })
+  const f = useFixture({
+    root: 'examples/starter-extra',
+    mode: 'build',
+    buildCommand: 'pnpm cf:build',
+    command: 'pnpm cf:preview',
+  })
   defineStarterTest(f)
 })
