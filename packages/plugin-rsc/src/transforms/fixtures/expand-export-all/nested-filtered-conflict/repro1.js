ef2ab8c69ed97413a73cc5fc61d671e0a0a745f7
Hiroshi Ogawa <4232207+hi-ogawa@users.noreply.github.com>
2026-06-01 13:17:51 +0900

    comment

--- packages/plugin-rsc/src/transforms/fixtures/expand-export-all/nested-filtered-conflict/repro1.js
+++ packages/plugin-rsc/src/transforms/fixtures/expand-export-all/nested-filtered-conflict/repro1.js
@@ -1,4 +1,7 @@
 // this doesn't fail but it should. potentially node/v8 bug.
 // node packages/plugin-rsc/src/transforms/fixtures/expand-export-all/nested-filtered-conflict/repro1.js
+// similar class of issue but different
+// https://github.com/nodejs/node/issues/53707
+// https://chromium.googlesource.com/v8/v8/+/88760dadfba1457aea76e50e6ead3b87f868c7fb
 import * as ns from './entry.js'
 console.log(ns.a)
